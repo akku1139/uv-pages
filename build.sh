2035eb3fab841bc20c4cc4747c61644a616a9711
-akku- <151517253+akku1139@users.noreply.github.com>
2024-01-16 23:34:42 +0900

    remove build step

--- build.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/sh
-wget "https://github.com/titaniumnetwork-dev/Ultraviolet/releases/download/v2.0.0/titaniumnetwork-dev-ultraviolet-2.0.0.tgz"
-tar zxvf "titaniumnetwork-dev-ultraviolet-2.0.0.tgz"
-
-cp package/dist/sw.js package/dist/uv.bundle.js package/dist/uv.client.js package/dist/uv.handler.js package/dist/uv.sw.js public/uv
