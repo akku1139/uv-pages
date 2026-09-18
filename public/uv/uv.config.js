d1d9d2f5522df7fca96b2ce61c148ceababf6e1d
Akari Tsuyukusa <akkun11.open@gmail.com>
2026-09-18 10:18:25 +0900

    Update uv.config.js

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -2,7 +2,7 @@
 
 self.__uv$config = {
   prefix: "/uv/service/",
-  wisp: "/wisp/",
+  wisp: "https://infrared-188.pages.dev/wisp/",
   bare: "https://infrared-188.pages.dev/bare/",
   encodeUrl: Ultraviolet.codec.xor.encode,
   decodeUrl: Ultraviolet.codec.xor.decode,
